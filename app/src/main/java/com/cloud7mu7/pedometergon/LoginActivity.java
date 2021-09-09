54ea0cc312172f5c8414d74fa3d643aa74764475
cloudmu7 <87072875+cloudmu7@users.noreply.github.com>
2021-09-10 04:57:02 +0900

    git210910
    04:57

--- app/src/main/java/com/cloud7mu7/pedometergon/LoginActivity.java
+++ app/src/main/java/com/cloud7mu7/pedometergon/LoginActivity.java
@@ -3,16 +3,19 @@ package com.cloud7mu7.pedometergon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
-    //Testing
+    public void log1(View view) {
+    }
+
+    public void log2(View view) {
+    }
 }
