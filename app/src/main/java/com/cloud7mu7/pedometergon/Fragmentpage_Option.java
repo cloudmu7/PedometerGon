7684e000d9a568978161bda0fd0971ed945fbfe5
cloudmu7 <87072875+cloudmu7@users.noreply.github.com>
2021-09-10 23:59:02 +0900

    last commet

---
+++ app/src/main/java/com/cloud7mu7/pedometergon/Fragmentpage_Option.java
@@ -0,0 +1,20 @@
+package com.cloud7mu7.pedometergon;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragmentpage_Option extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.option_fragement, container, false);
+    }
+}
--- app/src/main/java/com/cloud7mu7/pedometergon/MainActivity.java
+++ app/src/main/java/com/cloud7mu7/pedometergon/MainActivity.java
@@ -1,18 +1,70 @@
 package com.cloud7mu7.pedometergon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private BottomNavigationView bnv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bnv = findViewById(R.id.bnv_main);
+        bnv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                return true;
+            }
+        });
+        bnv.setSelectedItemId(R.id.navi_main);
     }
 
-    //Testing
+    private void BottomNavigate(int id){
+        String tag = String.valueOf(id);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment nowFrag = fragmentManager.getPrimaryNavigationFragment();
+        if(nowFrag != null){
+            fragmentTransaction.hide(nowFrag);
+        }
+
+        Fragment frag = fragmentManager.findFragmentByTag(tag);
+        if(frag == null){
+            if(id == R.id.navi_main){
+                frag = new Fragmentpage_Main();
+
+            }else if(id == R.id.navi_history){
+                frag = new Fragmentpage_History();
+
+            }else if(id == R.id.navi_friend){
+                frag = new Fragmentpage_Friend();
+
+            }else {
+                frag = new Fragmentpage_Option();
+            }
+
+            fragmentTransaction.add(R.id.content_layout, frag, tag);
+        }else{
+            fragmentTransaction.show(frag);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(frag);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+    }
+
 }
